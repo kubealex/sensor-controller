d55e13709646a916b278991bcca2ea8c4033b746
kubealex <al.rossi87@gmail.com>
2023-05-21 14:19:26 +0200

    change query

--- src/main/java/org/acme/model/InfluxQueries.java
+++ src/main/java/org/acme/model/InfluxQueries.java
@@ -2,7 +2,7 @@ package org.acme.model;
 
 public class InfluxQueries {
     public static String queryBuilder(String measurement, String deviceID, String location) {
-        String query = "from(bucket: \"devconf-demo\") |> range(start: -5m) |> filter(fn: (r) => r[\"_measurement\"] == \"SensorData\") |> filter(fn: (r) => r[\"_field\"] == \"" + measurement + "\") |> filter(fn: (r) => r[\"deviceID\"] == \"" + deviceID + "\") |> filter(fn: (r) => r[\"location\"] == \"" + location + "\") |> aggregateWindow(every: 5m, fn: mean, createEmpty: false) |> yield(name: \"mean\")";
+        String query = "from(bucket: \"devconf-demo\") |> range(start: -10m) |> filter(fn: (r) => r[\"_measurement\"] == \"SensorData\") |> filter(fn: (r) => r[\"_field\"] == \"" + measurement + "\") |> filter(fn: (r) => r[\"deviceID\"] == \"" + deviceID + "\") |> filter(fn: (r) => r[\"location\"] == \"" + location + "\") |> aggregateWindow(every: 1m, fn: mean, createEmpty: false) |> yield(name: \"mean\")";
         return query;
     }
 }
